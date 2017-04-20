653e9cc9dd85a0f623983210e547719de90fd66e
Adam Michniewski <adam.michniewski95@gmail.com>
2017-04-20 12:44:26 +0200

    Homework - restclient

--- src/main/java/wdsr/exercise3/client/ProductService.java
+++ src/main/java/wdsr/exercise3/client/ProductService.java
@@ -6,11 +6,21 @@ import java.util.Set;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import wdsr.exercise3.model.Product;
 import wdsr.exercise3.model.ProductType;
 
+
+
 public class ProductService extends RestClientBase {
+	
+	private String path = "products";
+	
 	protected ProductService(final String serverHost, final int serverPort, final Client client) {
 		super(serverHost, serverPort, client);
 	}
@@ -20,18 +30,22 @@ public class ProductService extends RestClientBase {
 	 * @param types Set of types to be looked up
 	 * @return A list of found products - possibly empty, never null.
 	 */
-	public List<Product> retrieveProducts(Set<ProductType> types) {
-		// TODO
-		return null;
+	public List<Product> retrieveProducts(Set<ProductType> types) 
+	{
+		WebTarget target = baseTarget.path(path);
+		List<Product> selectedProducts = target.queryParam("type", types.toArray()).request(MediaType.APPLICATION_JSON).get(new GenericType<List<Product>>(){});
+		return selectedProducts;
 	}
 	
 	/**
 	 * Looks up all products known to the server.
 	 * @return A list of all products - possibly empty, never null.
 	 */
-	public List<Product> retrieveAllProducts() {
-		// TODO
-		return null;
+	public List<Product> retrieveAllProducts() 
+	{
+		WebTarget target = baseTarget.path(path);
+		List<Product> allProducts = target.request(MediaType.APPLICATION_JSON).get(new GenericType<List<Product>>(){});
+		return allProducts;
 	}
 	
 	/**
@@ -40,9 +54,11 @@ public class ProductService extends RestClientBase {
 	 * @return Product if found
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
-	public Product retrieveProduct(int id) {
-		// TODO
-		return null;
+	public Product retrieveProduct(int id) 
+	{
+		WebTarget target = baseTarget.path(path + "/" + id);
+		Product p = target.request(MediaType.APPLICATION_JSON).get(Product.class);
+		return p;
 	}	
 	
 	/**
@@ -51,9 +67,14 @@ public class ProductService extends RestClientBase {
 	 * @return ID of the new product.
 	 * @throws WebApplicationException if request to the server failed
 	 */
-	public int storeNewProduct(Product product) {
-		// TODO
-		return 0;
+	public int storeNewProduct(Product product) 
+	{
+		WebTarget target = baseTarget.path(path);
+		Response r = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(product, MediaType.APPLICATION_JSON), Response.class);
+		if (r.getStatus() == Response.Status.CREATED.getStatusCode())
+			throw new WebApplicationException();
+		int id = r.readEntity(Product.class).getId();
+		return id;
 	}
 	
 	/**
@@ -61,8 +82,12 @@ public class ProductService extends RestClientBase {
 	 * @param product Product with updated values. Its ID must identify an existing resource.
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
-	public void updateProduct(Product product) {
-		// TODO
+	public void updateProduct(Product product) 
+	{
+		WebTarget target = baseTarget.path(path + "/" + product.getId());
+		Response r = target.request(MediaType.APPLICATION_JSON).put(Entity.entity(product, MediaType.APPLICATION_JSON), Response.class);
+		if ( r.getStatus() == Response.Status.NOT_FOUND.getStatusCode())
+			throw new NotFoundException();
 	}
 
 	
@@ -71,7 +96,11 @@ public class ProductService extends RestClientBase {
 	 * @param product Product to be deleted. Its ID must identify an existing resource.
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
-	public void deleteProduct(Product product) {
-		// TODO
+	public void deleteProduct(Product product) 
+	{
+		WebTarget target = baseTarget.path(path + "/" + product.getId());
+		Response r = target.request(MediaType.APPLICATION_JSON).delete(Response.class);
+		if (r.getStatus() == Response.Status.NOT_FOUND.getStatusCode())
+			throw new NotFoundException();
 	}
 }
